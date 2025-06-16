7fca63e488ea118fbaf85f159e47f3bc96c800c0
Justtest-ing <iamholala@gmail.com>
2025-06-16 13:04:31 +0600

    scripte to install zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -15,18 +15,24 @@ chsh -s $(which zsh)
 function install_ohmyzsh() {
     echo "Installing Oh My Zsh..."
     for i in {1..5}; do
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && break || sleep 5
+        if sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+            echo "Oh My Zsh installed successfully!"
+            exit 0  # Exit with success after successful installation
+        fi
+        sleep 5
     done
+    echo "Failed to install Oh My Zsh after multiple attempts."
+    exit 1  # Exit with failure if all retries fail
 }
 
-install_ohmyzsh
+install_ohmyzsh || { echo "Proceeding with the rest of the script..."; }
 
 # Install ZSH plugins
 echo "Installing ZSH plugins..."
 mkdir -p ~/.oh-my-zsh/plugins
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Set up custom .zhsrc if desired
 echo "Would you like to set up a custom .zhsrc configuration? (y/n) "
