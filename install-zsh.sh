676fb5cf05953ace0c9da0c5df8f3e91f9ca950d
Justtest-ing <iamholala@gmail.com>
2025-06-16 13:09:36 +0600

    scripte to install zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -11,6 +11,13 @@ sudo apt install zsh
 echo "Setting ZSH as default shell..."
 chsh -s $(which zsh)
 
+# Set up custom .zhsrc if desired
+echo "Would you like to set up a custom .zhsrc configuration? (y/n) "
+read setup_config
+if [[ $setup_config == "y" ]]; then
+    curl -o ~/.zhsrc https://raw.githubusercontent.com/justtest-ing/user-script/refs/heads/main/install-zsh.zhsrc.example
+fi
+
 # Install Oh My Zsh with retry logic
 function install_ohmyzsh() {
     echo "Installing Oh My Zsh..."
@@ -34,12 +41,6 @@ mkdir -p ~/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-# Set up custom .zhsrc if desired
-echo "Would you like to set up a custom .zhsrc configuration? (y/n) "
-read setup_config
-if [[ $setup_config == "y" ]]; then
-    curl -o ~/.zhsrc https://raw.githubusercontent.com/justtest-ing/user-script/refs/heads/main/install-zsh.zhsrc.example
-fi
 
 echo "ZSH installation completed!"
 echo "You may need to log out and back in for changes to take effect."
